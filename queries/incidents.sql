62f3268572231a0d979c7aab675209078adeb10b
David Montaño <davidmr.d@gmail.com>
2021-12-24 16:05:28 -0500

    Update incidents to handle "incident-reporter" source

--- queries/incidents.sql
+++ queries/incidents.sql
@@ -7,20 +7,45 @@ MAX(time_resolved) as time_resolved,
 ARRAY_AGG(root_cause IGNORE NULLS) changes,
 FROM
 (
-SELECT 
-source,
-CASE WHEN source LIKE "github%" THEN JSON_EXTRACT_SCALAR(metadata, '$.issue.number')
-     WHEN source LIKE "gitlab%" AND event_type = "note" THEN JSON_EXTRACT_SCALAR(metadata, '$.object_attributes.noteable_id')
-     WHEN source LIKE "gitlab%" AND event_type = "issue" THEN JSON_EXTRACT_SCALAR(metadata, '$.object_attributes.id') end as incident_id,
-TIMESTAMP(CASE WHEN source LIKE "github%" THEN JSON_EXTRACT_SCALAR(metadata, '$.issue.created_at')
-     WHEN source LIKE "gitlab%" THEN JSON_EXTRACT_SCALAR(metadata, '$.object_attributes.created_at') end) as time_created,
-TIMESTAMP(CASE WHEN source LIKE "github%" THEN JSON_EXTRACT_SCALAR(metadata, '$.issue.closed_at')
-     WHEN source LIKE "gitlab%" THEN JSON_EXTRACT_SCALAR(metadata, '$.object_attributes.closed_at') end) as time_resolved,
-REGEXP_EXTRACT(metadata, r"root cause: ([[:alnum:]]*)") as root_cause,
-CASE WHEN source LIKE "github%" THEN REGEXP_CONTAINS(JSON_EXTRACT(metadata, '$.issue.labels'), '"name":"Incident"')
-     WHEN source LIKE "gitlab%" THEN REGEXP_CONTAINS(JSON_EXTRACT(metadata, '$.object_attributes.labels'), '"title":"Incident"') end as bug,
-FROM four_keys.events_raw 
-WHERE event_type LIKE "issue%" OR (event_type = "note" and JSON_EXTRACT_SCALAR(metadata, '$.object_attributes.noteable_type') = 'Issue')
+    SELECT source,
+       metadata,
+       CASE
+           WHEN source LIKE "github%" THEN JSON_EXTRACT_SCALAR(metadata, '$.issue.number')
+           WHEN source LIKE "gitlab%" AND event_type = "note"
+               THEN JSON_EXTRACT_SCALAR(metadata, '$.object_attributes.noteable_id')
+           WHEN source LIKE "gitlab%" AND event_type = "issue"
+               THEN JSON_EXTRACT_SCALAR(metadata, '$.object_attributes.id')
+           WHEN source LIKE "incident-reporter" AND event_type = "incident"
+               THEN JSON_EXTRACT_SCALAR(metadata, '$.id')
+           END                                                                                   AS incident_id,
+       TIMESTAMP(CASE
+                     WHEN source LIKE "github%" THEN JSON_EXTRACT_SCALAR(metadata, '$.issue.created_at')
+                     WHEN source LIKE "gitlab%"
+                         THEN JSON_EXTRACT_SCALAR(metadata, '$.object_attributes.created_at')
+                     WHEN source LIKE "incident-reporter" AND event_type = "incident"
+                         THEN JSON_EXTRACT_SCALAR(metadata, '$.created_time') END)               AS time_created,
+       TIMESTAMP(CASE
+                     WHEN source LIKE "github%" THEN JSON_EXTRACT_SCALAR(metadata, '$.issue.closed_at')
+                     WHEN source LIKE "gitlab%"
+                         THEN JSON_EXTRACT_SCALAR(metadata, '$.object_attributes.closed_at')
+                     WHEN source LIKE "incident-reporter" AND event_type = "incident"
+                         THEN JSON_EXTRACT_SCALAR(metadata,
+                                                  "$.properties['Solved time'].date.start") END) as time_resolved,
+       CASE
+           WHEN source LIKE "incident-reporter" AND event_type = "incident"
+               THEN JSON_EXTRACT_SCALAR(metadata, "$.properties['Introduced at (commit)'].plain_text")
+           ELSE REGEXP_EXTRACT(metadata, r"root cause: ([[:alnum:]]*)") END                      as root_cause,
+       CASE
+           WHEN source LIKE "github%" THEN REGEXP_CONTAINS(JSON_EXTRACT(metadata, '$.issue.labels'),
+                                                           '"name":"Incident"')
+           WHEN source LIKE "gitlab%" THEN REGEXP_CONTAINS(JSON_EXTRACT(metadata, '$.object_attributes.labels'),
+                                                           '"title":"Incident"')
+           WHEN source LIKE "incident-reporter" AND event_type = "incident" THEN
+                   JSON_EXTRACT_SCALAR(metadata, '$.properties.Priority.select.name') IN ("High", "Highest")
+           END                                                                                   as bug,
+    FROM four_keys.events_raw
+    WHERE event_type = "incident"
+      AND source = "incident-reporter"
 ) issue
 LEFT JOIN (SELECT time_created, changes FROM four_keys.deployments d, d.changes) root on root.changes = root_cause
 GROUP BY 1,2
