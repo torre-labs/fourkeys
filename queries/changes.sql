67d5710cebd62ace186402d62f6a81c23a493553
David Montaño <davidmr.d@gmail.com>
2022-02-25 11:19:46 -0500

    Update changes view to better measure when a commit was created
    To have more precise metrics it'll measure the creation of a commit as the first time it was available for review (PR). This way, we avoid changing our processes as a whole.
    
    The issue is that when merging a PR, it creates a new commit that seems to be created just at the time of merge. To measure lead time, it's better to consider the time it took to review the PR. For that, it sets the time of the merge commit as the PR creation time.

--- queries/changes.sql
+++ queries/changes.sql
@@ -1,10 +1,31 @@
 # Changes Table
-SELECT 
-source,
-event_type,
-JSON_EXTRACT_SCALAR(commit, '$.id') change_id,
-TIMESTAMP_TRUNC(TIMESTAMP(JSON_EXTRACT_SCALAR(commit, '$.timestamp')),second) as time_created,
-FROM four_keys.events_raw e,
-UNNEST(JSON_EXTRACT_ARRAY(e.metadata, '$.commits')) as commit
-WHERE event_type = "push"
-GROUP BY 1,2,3,4
+WITH `changes_made_via_pr` AS (
+    SELECT `source`,
+           `event_type`,
+           JSON_EXTRACT_SCALAR(`metadata`, '$.pull_request.merge_commit_sha') AS `change_id`,
+           TIMESTAMP_TRUNC(TIMESTAMP(JSON_EXTRACT_SCALAR(`metadata`, '$.pull_request.created_at')),
+                           SECOND)                                            AS `time_created`
+    FROM `four_keys`.`events_raw`
+    WHERE `event_type` = 'pull_request'
+      AND JSON_EXTRACT_SCALAR(`metadata`, '$.pull_request.state') = 'closed'
+    GROUP BY 1, 2, 3, 4
+),
+     `changes_without_pr` AS (
+         SELECT `source`,
+                `event_type`,
+                JSON_EXTRACT_SCALAR(`commit`, '$.id')                                               `change_id`,
+                TIMESTAMP_TRUNC(TIMESTAMP(JSON_EXTRACT_SCALAR(`commit`, '$.timestamp')), SECOND) AS `time_created`,
+         FROM `four_keys`.`events_raw` `e`,
+              UNNEST(JSON_EXTRACT_ARRAY(`e`.`metadata`, '$.commits')) AS `commit`
+         WHERE `event_type` = "push"
+         GROUP BY 1, 2, 3, 4
+     ),
+     `all_changes` AS (
+         SELECT *
+         FROM `changes_made_via_pr`
+         UNION ALL
+         SELECT *
+         FROM `changes_without_pr`
+         WHERE `change_id` NOT IN (SELECT `change_id` FROM `changes_made_via_pr`))
+SELECT *
+FROM `all_changes`;
